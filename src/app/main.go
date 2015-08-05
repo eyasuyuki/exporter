7286754c82c556db2d6b4e186e9b456d1cff1656
Yasuyuki ENDO <eyasuyuki@gmail.com>
2015-08-05 17:32:56 +0900

    add TableMete,ColumnMeta

--- src/app/main.go
+++ src/app/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	_ "github.com/lib/pq"
 	"database/sql"
+	"text/template"
+	"bytes"
 	"fmt"
 	"conf"
 	"log"
@@ -11,14 +13,39 @@ import (
 var c *conf.Conf
 
 const SELECT_TABLE_NAMES = `
-SELECT table_name
-  FROM information_schema.tables
- WHERE table_schema='public'
-   AND table_type='BASE TABLE'
+SELECT
+	table_name
+FROM
+	information_schema.tables
+WHERE
+	table_schema='public'
+	AND table_type='BASE TABLE'
 `
+
+const SELECT_ALL = `
+SELECT * FROM {{.TableName}}
+`
+var TMPL *template.Template
+
+type ColumnMeta struct {
+	ColumnName string `json:"column_name"`
+	ColumnType string `json:"column_type"`
+}
+
+type TableMeta struct {
+	TableName string `json:"table_name"`
+	Columns []ColumnMeta
+}
+
+func (t *TableMeta)MakeQuery() (string, error) {
+	var query bytes.Buffer
+	err := TMPL.Execute(&query, t)
+	return query.String(), err
+}
 
 func init() {
 	c = conf.NewConf("./conf.json")
+	TMPL = template.Must(template.New("query").Parse(SELECT_ALL))
 }
 
 func main() {
@@ -42,6 +69,23 @@ func main() {
 		var table_name string
 		rows.Scan(&table_name)
 		fmt.Println(table_name)
+		meta := new(TableMeta)
+		meta.TableName = table_name
+		query, _ := meta.MakeQuery()
+		fmt.Print(query)
+		res, err := conn.Query(query)
+		if err != nil {
+			log.Fatalf("error: %v", err);
+		}
+		defer res.Close()
+		columns, err := res.Columns()
+		if err != nil {
+			log.Fatalf("error: %v", err);
+		}
+		fmt.Printf("columns=%d", len(columns))
+		for c := range columns {
+			fmt.Printf("c = %V", c)
+		}
 	}
 
 }
